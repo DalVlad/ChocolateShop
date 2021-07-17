ff2920d19b5d3d4a2e8afa4ebbe4c82d40b2ad32
Vlad <vladbulanov47@gmail.com>
2021-07-17 18:35:21 +0300

    minor changes

--- src/main/java/shop/dao/ChocolateDAO.java
+++ src/main/java/shop/dao/ChocolateDAO.java
@@ -1,8 +1,9 @@
 package shop.dao;
 
 import org.springframework.stereotype.Component;
-import shop.model.Chocolate;
-import shop.model.ChocolateColor;
+import shop.model.chocolate.Chocolate;
+import shop.model.chocolate.ChocolateColor;
+import shop.model.people.Client;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,28 +11,29 @@ import java.util.List;
 @Component
 public class ChocolateDAO {
 
-    private static int INDEX;
+    private static int INDEX_CHOCOLATE;
     private List<Chocolate> chocolates;
 
     {
         chocolates = new ArrayList<>();
 
-        chocolates.add(new Chocolate(++INDEX, ChocolateColor.BLACK, 50, "Marks"));
-        chocolates.add(new Chocolate(++INDEX, ChocolateColor.BLACK, 90, "Alpen Red"));
-        chocolates.add(new Chocolate(++INDEX, ChocolateColor.WHITE, 20, "Milca"));
-        chocolates.add(new Chocolate(++INDEX, ChocolateColor.WHITE, 30, "Mequik"));
+        chocolates.add(new Chocolate(++INDEX_CHOCOLATE, ChocolateColor.BLACK, 50, "Marks"));
+        chocolates.add(new Chocolate(++INDEX_CHOCOLATE, ChocolateColor.BLACK, 90, "Alpen Red"));
+        chocolates.add(new Chocolate(++INDEX_CHOCOLATE, ChocolateColor.WHITE, 20, "Milca"));
+        chocolates.add(new Chocolate(++INDEX_CHOCOLATE, ChocolateColor.WHITE, 30, "Mequik"));
     }
+
 
     public List<Chocolate> allChocolate(){
         return chocolates;
     }
 
-    public Chocolate getChocolate(final int index){
+    public Chocolate getChocolate(int index){
         return chocolates.stream().filter(chocolate -> chocolate.getIndex() == index).findAny().orElse(null);
     }
 
     public void save(Chocolate chocolate){
-        chocolate.setIndex(++INDEX);
+        chocolate.setIndex(++INDEX_CHOCOLATE);
         chocolates.add(chocolate);
     }
 
