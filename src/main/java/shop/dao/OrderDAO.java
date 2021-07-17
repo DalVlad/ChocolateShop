9372ef9e263d55df0e9b03add54c71ed045aed21
Vlad <vladbulanov47@gmail.com>
2021-07-17 18:39:47 +0300

    DAO for orders

---
+++ src/main/java/shop/dao/OrderDAO.java
@@ -0,0 +1,32 @@
+package shop.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import shop.model.basket.Basket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class OrderDAO {
+
+    private final ChocolateDAO chocolateDAO;
+    @Autowired
+    public OrderDAO(ChocolateDAO chocolateDAO) {
+        this.chocolateDAO = chocolateDAO;
+    }
+
+    private List<Basket> basket;
+    {
+        basket = new ArrayList<>();
+    }
+
+    public void save(int[] basket){
+        Basket basket1 = new Basket(basket.length);
+        for(int i: basket){
+            basket1.addObjectChocolates(chocolateDAO.getChocolate(i));
+        }
+        this.basket.add(basket1);
+    }
+}
