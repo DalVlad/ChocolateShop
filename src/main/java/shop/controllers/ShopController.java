10895f7536e5a467574c8a7267660c53917c5a74
Vlad <vladbulanov47@gmail.com>
2021-07-17 18:43:55 +0300

    Added order processing

--- src/main/java/shop/controllers/ShopController.java
+++ src/main/java/shop/controllers/ShopController.java
@@ -5,16 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import shop.dao.ChocolateDAO;
-import shop.model.Chocolate;
+import shop.dao.OrderDAO;
+import shop.dao.PeopleDAO;
+import shop.model.basket.Basket;
+import shop.model.chocolate.Chocolate;
+import shop.model.people.Client;
+
+import java.util.ArrayList;
+
 
 @Controller
 public class ShopController {
 
     private final ChocolateDAO chocolateDAO;
+    private final PeopleDAO peopleDAO;
+    private final OrderDAO orderDAO;
 
     @Autowired
-    public ShopController(ChocolateDAO chocolateDAO) {
+    public ShopController(ChocolateDAO chocolateDAO, PeopleDAO peopleDAO, OrderDAO orderDAO) {
         this.chocolateDAO = chocolateDAO;
+        this.peopleDAO = peopleDAO;
+        this.orderDAO = orderDAO;
     }
 
     @GetMapping("/home")
@@ -41,6 +52,19 @@ public class ShopController {
         return "redirect:/home";
     }
 
+    @GetMapping("/order")
+    public String order(Model model){
+        model.addAttribute("chocolates", chocolateDAO.allChocolate());
+        model.addAttribute("list", new ArrayList<Integer>());
+//        model.addAttribute("client", new Client());
+        return "order";
+    }
+
+    @PostMapping("/orderAccepted")
+    public String orderAccepted(@RequestParam("list") int[] chocolate){
+        orderDAO.save(chocolate);
+        return "redirect:/home";
+    }
 
 
 }
